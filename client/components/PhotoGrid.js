4651c720e695cb5d678b9ba5082a8873bac6a27e
Heloá Meneses <heloa.meneses@gmail.com>
2017-01-29 21:15:14 -0200

    Adding style and wiring the components with their content

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Photo from './Photo';
 
 class PhotoGrid extends Component {
   render() {
     return (
       <div className="photo-grid">
-        I'm the PhotoGrid
+        {this.props.posts.map((post, i) => <Photo {...this.props} key={i} i={i} post={post} />)}
       </div>
     )
   }
